2ce4c2df8a6c4ef366df43c6ed827a4877cb490d
Illya Havsiyevych <illya_havsiyevych@epam.com>
2014-04-12 19:37:07 +0300

    CJ 2014 qualification

--- src/main/java/codejam2014/qualification/MinesweeperMaster.java
+++ src/main/java/codejam2014/qualification/MinesweeperMaster.java
@@ -11,7 +11,7 @@ public class MinesweeperMaster {
     private static final String ROUND = "codejam2014/qualification";
 
     private static final String SAMPLE = "C-sample.in";
-    private static final String SMALL = "C-small-attempt0.in";
+    private static final String SMALL = "C-small-attempt1.in";
     private static final String LARGE = "C-large.in";
 
     private Scanner scanner;
@@ -73,7 +73,7 @@ public class MinesweeperMaster {
     public static void main(String[] args) {
         try {
             runTest(SAMPLE, true);
- //           runTest(SMALL, false);
+            runTest(SMALL, false);
 //            runTest(LARGE, false);
         } catch (Exception e) {
             e.printStackTrace();
@@ -82,17 +82,247 @@ public class MinesweeperMaster {
 
     // problem part
 
+    private int r, c, m;
+
     /**
      * Solve the problem
      */
     public void solve() {
-        // 1 <= T <= 1000
+        // 1 <= T <= 230
         int t = scanner.nextInt();
         scanner.nextLine();
 
         for (int i = 1; i <= t; i++) {
             writer.print("Case #");
-            writer.print(i + ": ");
+            writer.println(i + ": ");
+
+            // 1 <= R, C <= 50
+            r = scanner.nextInt();
+            c = scanner.nextInt();
+
+            // 0 <= M < R * C
+            m = scanner.nextInt();
+
+            writer.print(brute(r, c, m));
        }
+    }
+
+    public String brute(int r, int c, int m) {
+        Mine[] mines = new Mine[m];
+        Mine mine = new Mine(0, 0);
+        fill(mines, mine, 0);
+
+        Minesweeper minesweeper = new Minesweeper(mines);
+        while(true) {
+            System.out.println(minesweeper.toString());
+
+            for (int i=0; i<r; i++)
+                for (int j=0; j<c; j++) {
+                    if (minesweeper.hasMine(i, j))
+                        continue;
+
+                    if (minesweeper.click(i, j))
+                        return minesweeper.toString(i, j);
+                }
+
+            if (!minesweeper.hasNext())
+                return "Impossible\n";
+
+            minesweeper = minesweeper.next();
+        }
+    }
+
+    private void fill(Mine[] mines, Mine mine, int k) {
+        for(int i=k; i<m; i++) {
+            mines[i] = mine;
+            if (i != m-1)
+                mine = mine.next();
+        }
+    }
+
+    private class Mine {
+        int i, j;
+
+        private Mine(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public boolean hasNext() {
+            return (i != r-1) || (j != c-1);
+        }
+
+        public Mine next() {
+            if (!hasNext())
+                throw new IllegalStateException();
+
+            if (j+1 < c)
+                return new Mine(i, j+1);
+            return new Mine(i+1, 0);
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Mine mine = (Mine) o;
+
+            if (i != mine.i) return false;
+            if (j != mine.j) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = i;
+            result = 31 * result + j;
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + i +"," + j + "}";
+        }
+    }
+
+    private class Minesweeper {
+        private Mine[] mines;
+        private Set<Mine> mineSet;
+
+        public Minesweeper(Mine[] mines) {
+            this.mines = mines;
+            this.mineSet = new HashSet<Mine>(Arrays.asList(mines));
+        }
+
+        public boolean hasNext() {
+            int index = m-1;
+
+            for(int i=r-1; i>=0; i--)
+                for(int j=c-1; j>=0; j--) {
+                    if (mines[index].equals(new Mine(i, j))) {
+                        if (index == 0)
+                            return false;
+                        index--;
+                    } else
+                        return true;
+                }
+            return true;
+        }
+
+        public Minesweeper next() {
+            int index = m-1;
+
+            for(int i=r-1; i>=0; i--)
+                for(int j=c-1; j>=0; j--) {
+                    if (mines[index].equals(new Mine(i, j))) {
+                        index--;
+                    } else {
+                        Mine[] copy = Arrays.copyOf(mines, mines.length);
+                        fill(copy, copy[index].next(), index);
+                        return new Minesweeper(copy);
+                    }
+                }
+            throw new IllegalStateException();
+        }
+
+        public boolean hasMine(int i, int j) {
+            return mineSet.contains(new Mine(i, j));
+        }
+
+        public boolean click(int i, int j) {
+            boolean[][] visited = new boolean[r][c];
+            for(int k=0; k<r; k++)
+                for(int l=0; l<c; l++)
+                    visited[k][l] = false;
+
+            visit(i, j, visited);
+
+            for(int k=0; k<r; k++)
+                for(int l=0; l<c; l++)
+                    if ((!visited[k][l]) && (!hasMine(k, l)))
+                        return false;
+
+            return true;
+        }
+
+        private void visit(int i, int j, boolean[][] visited) {
+            if ((i<0) || (i>=r))
+                return;
+            if ((j<0) || (j>=c))
+                return;
+            if (visited[i][j])
+                return;
+
+            visited[i][j] = true;
+
+            Iterable<Mine> neighbors = neighbors(i, j);
+            if (count(neighbors) == 0) {
+                for (Mine mine : neighbors)
+                    visit(mine.i, mine.j, visited);
+            }
+        }
+
+        private int count(Iterable<Mine> neighbors) {
+            int mines = 0;
+            for(Mine mine: neighbors)
+                if (mineSet.contains(mine))
+                    mines++;
+            return mines;
+        }
+
+        public Iterable<Mine> neighbors(int i, int j) {
+            List<Mine> mines = new LinkedList<Mine>();
+
+            addIfOk(mines, i-1, j-1);
+            addIfOk(mines, i-1, j);
+            addIfOk(mines, i-1, j+1);
+            addIfOk(mines, i, j-1);
+            addIfOk(mines, i, j+1);
+            addIfOk(mines, i+1, j-1);
+            addIfOk(mines, i+1, j);
+            addIfOk(mines, i+1, j+1);
+
+            return mines;
+        }
+
+        private void addIfOk(List<Mine> mines, int i, int j) {
+            if ((i >= 0) && (i < r) && (j >= 0) && (j < c))
+                mines.add(new Mine(i, j));
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+
+            for(int k=0; k<r; k++) {
+                for (int l = 0; l < c; l++) {
+                    if (hasMine(k, l))
+                        sb.append("*");
+                    else
+                        sb.append(".");
+                }
+                sb.append("\n");
+            }
+            return sb.toString();
+        }
+
+        public String toString(int i, int j) {
+            StringBuilder sb = new StringBuilder();
+
+            for(int k=0; k<r; k++) {
+                for (int l = 0; l < c; l++) {
+                    if (hasMine(k, l))
+                        sb.append("*");
+                    else if ((i == k) && (j == l))
+                        sb.append("c");
+                    else
+                        sb.append(".");
+                }
+                sb.append("\n");
+            }
+            return sb.toString();
+        }
     }
 }
